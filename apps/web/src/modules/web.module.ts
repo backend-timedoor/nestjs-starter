4646ac6ac24e00a1e4db496fa10c30d50b501742
rizkynear <rizkynear1@gmail.com>
2022-04-21 09:28:08 +0800

    implement config

--- apps/web/src/modules/web.module.ts
+++ apps/web/src/modules/web.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from 'config/app-config.module';
+import { DatabaseConfigService } from 'config/services/database-config.service';
 import { HomeModule } from './home/home.module';
 
 const modules = [
-  HomeModule
+    HomeModule
 ];
 
 @Module({
-  imports: [
-    ...modules
-  ]
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['.env']
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [AppConfigModule],
+            inject: [DatabaseConfigService],
+            useFactory: (configService: DatabaseConfigService) => configService.mysqlConfig
+        }),
+        ...modules
+    ]
 })
-export class WebModule {}
+export class WebModule { }
